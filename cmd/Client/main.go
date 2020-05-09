3a80a37547132b698287ac21dddc10e663ea883d
mradrianhh <hansen.adrian.hardy@gmail.com>
2020-05-09 10:35:48 +0200

    Transfer to desktop.

--- cmd/Client/main.go
+++ cmd/Client/main.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/gob"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/mradrianhh/go-multiplayer-fighter-game-client/pkg/models"
+)
 
 func main() {
-	fmt.Println("Hello from client!")
+	player := models.NewPlayer("Adrianhh")
 
-	fmt.Scanln()
+	service := "0.0.0.0:1200"
+
+	conn, err := net.Dial("tcp", service)
+	checkError(err)
+
+	encoder := gob.NewEncoder(conn)
+	decoder := gob.NewDecoder(conn)
+
+	encoder.Encode(player)
+
+	var newPlayer models.Player
+	decoder.Decode(&newPlayer)
+
+	fmt.Println(newPlayer.Exp)
+
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
